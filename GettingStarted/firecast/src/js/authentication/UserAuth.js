5e82a291ddb737b103e23897ca27273be193087b
haglermgp <hagler.mijail@gmail.com>
2017-11-15 10:51:31 -0500

    [Add] Auth

--- GettingStarted/firecast/src/js/authentication/UserAuth.js
+++ GettingStarted/firecast/src/js/authentication/UserAuth.js
@@ -7,54 +7,21 @@ export default class UserCollection extends Component {
   constructor(){
     super()
     this.state = {
-      // form values
-      form_first: '',
-      form_last: '',
-      form_born: '',
-
-      // general config
-      e_status_sandwich: '',
-      DB: '',
-      e_load_status: {}
+      
     }
 
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleStatusName = this.handleStatusName.bind(this)
   }
 
   componentWillMount () {
-    const db = firebase.firestore() 
-    // const DB = firestore.collection('samples').doc('sandiwchData').collection('condiments').doc('relish').collection('options').doc(...)
-    const v_docRef = db.collection('users')
-    this.setState({
-      DB: v_docRef
-    })
 
   }
 
   componentDidMount () {
-    this.state.DB.get()
-      .then((doc) => {
-        this.setState({
-          e_load_status: doc
-        })
-      })
 
   }
 
   handleSubmit (e) {
     e.preventDefault()
-    console.log('I am going to save ' + this.state.e_status_sandwich + ' to Firestore')
-
-    this.state.DB.add({
-      fist: this.state.form_first,
-      last: this.state.form_last,
-      born: this.state.form_born
-    }).then( (docRef) => {
-      console.log('Document written with ID', docRef.id)
-    }).catch( (error) => {
-      console.log('Error adding document:', error)
-    })
   
   }
 
@@ -73,11 +40,7 @@ export default class UserCollection extends Component {
             {/*<button onClick={this.handleLoad} >Load</button>*/}
           </form>
         </div>
-{/*        <div>
-          <span><b>List of User</b></span>
-          <ListUsers dataProp={this.state.e_load_status}/>
-        </div>
-*/}      </div>
+    </div>
     );
   }
 
@@ -86,14 +49,3 @@ export default class UserCollection extends Component {
   }
 
 }
-
-
-// const ListUsers = (props) => {
-//   debugger
-//   props ? 
-//   return props.dataProp.forEach((data, index) => { 
-//     <div key={index} >
-//       <span>{data.id}</span>
-//     </div>
-//   }) : 'upss!, we dont have user :('
-// }
